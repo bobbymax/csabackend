9b047d0ea75b63e398ed3b4878d751af414c842e
Bobby Ekaro <bobby.ekaro@ncdmb.gov.ng>
2023-11-21 10:58:19 +0100

    cors:updates

--- config/cors.php
+++ config/cors.php
@@ -6,7 +6,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://localhost:3000', 'https://csa.ncdmb.gov.ng'],
 
     'allowed_origins_patterns' => [],
 
